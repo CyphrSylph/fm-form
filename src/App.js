78661d23693fbeb15dbd47aa7df3ccdeab9ba4b0
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-02-18 12:35:59 -0600

    removed divs wrapping inputs and updated form styling

--- src/App.js
+++ src/App.js
@@ -12,18 +12,15 @@ function App() {
 				<div className="signup__content">
 					<button href='#signup' className="button flex__button"><b>Try it free 7 days</b> then $20/mo. thereafter</button>
 					<form className="signup__form">
-						<div className="signup__form-div">
+						
 							<input type="text" name="first name" className="signup__form-input" placeholder="First Name" />
-						</div>
-						<div className="signup__form-div">
+						
 							<input type="text" name="last name" className="signup__form-input" placeholder="Last Name" />
-						</div>
-						<div className="signup__form-div">
+						
 							<input type="email" name="email" className="signup__form-input" placeholder="Email Address" />
-						</div>
-						<div className="signup__form-div">
+						
 							<input type="text" name="password" className="signup__form-input" placeholder="Password" />
-						</div>
+						
 						<button href="#signup" className="button submit__button">Claim your free trial</button>
 						<p className="form__terms">By clicking the button, you are agreeing to our <a href='https://www.iubenda.com/terms-and-conditions/68783624' className="form__term-link"><b>Terms and Services</b></a>.</p>
 					</form>
